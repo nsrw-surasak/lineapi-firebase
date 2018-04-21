6a4d29be30d8d37e486e2adb3d9a52ee1cc3e763
nsrw-surasak <nsrw.surasak@gmail.com>
2018-04-21 20:02:17 +0700

    Add heroku and app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict'
+const line = require('node-line-bot-api')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// need raw buffer for signature validation
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
+  }
+}))
+
+// init with auth
+line.init({
+  accessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
+  // (Optional) for webhook signature validation
+  channelSecret: process.env.LINE_BOT_CHANNEL_SECRET
+});
+
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
+
+app.listen(process.env.PORT || 80, () => {
+  console.log('Example app listening on port 80!')
+})
